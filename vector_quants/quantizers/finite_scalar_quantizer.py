2e8301bfdc546e34e3c940b26132ac9f53135b94
Doraemonzzz <715073608@qq.com>
2024-06-25 07:16:20 +0000

    fix fsq bug

--- vector_quants/quantizers/finite_scalar_quantizer.py
+++ vector_quants/quantizers/finite_scalar_quantizer.py
@@ -32,15 +32,18 @@ class FiniteScalarQuantizer(BaseVectorQuantizer):
         return x + (xhat - x).detach()
 
     def forward(self, x):
-        # get indice
-        indice = self.latent_to_indice(x)
-
-        # quantize
-        x_quant = self.indice_to_code(indice)
-
+        x_quant, indice = self.latent_to_code_and_indice(x)
         diff = torch.tensor(0.0).cuda().float()
 
         return x_quant, diff, indice
+
+    def latent_to_code_and_indice(self, latent):
+        d = self._levels - 1
+        number = self.round_ste(F.sigmoid(latent) * d)
+        code = number / d
+        indice = (number * self._basis).sum(dim=-1).to(torch.int32)
+
+        return code, indice
 
     def latent_to_indice(self, latent):
         # (b, *, d) -> (n, d)
@@ -59,3 +62,78 @@ class FiniteScalarQuantizer(BaseVectorQuantizer):
         code = code / (self._levels - 1)
 
         return code
+
+    # ###### v1
+    # def forward(self, x):
+    #     # get indice
+    #     indice = self.latent_to_indice(x)
+
+    #     # quantize
+    #     x_quant = self.indice_to_code(indice)
+
+    #     diff = torch.tensor(0.0).cuda().float()
+
+    #     return x_quant, diff, indice
+
+    # def latent_to_indice(self, latent):
+    #     # (b, *, d) -> (n, d)
+    #     latent, ps = pack_one(latent, "* d")
+    #     number = self.round_ste(F.sigmoid(latent) * (self._levels - 1))
+    #     indice = (number * self._basis).sum(dim=-1).to(torch.int32)
+
+    #     indice = unpack_one(indice, ps, "*")
+
+    #     return indice
+
+    # def indice_to_code(self, indice):
+    #     # (..., d)
+    #     code = (indice.unsqueeze(-1) // self._basis) % self._levels
+    #     # convert to [0, 1]
+    #     code = code / (self._levels - 1)
+
+    #     return code
+    # ###### v1
+
+    # ###### V2
+    # def bound(self, z: Tensor, eps: float = 1e-3) -> Tensor:
+    #     """Bound `z`, an array of shape (..., d)."""
+    #     d = self._levels - 1
+    #     # [0, L - 1]
+    #     output = d * F.sigmoid(z)
+    #     return output
+
+    # def quantize(self, z: Tensor) -> Tensor:
+    #     """Quantizes z, returns quantized zhat, same shape as z."""
+    #     d = self._levels - 1
+    #     # [0, L - 1] -> [0, 1]
+    #     quantized = self.round_ste(self.bound(z)) / d
+    #     return quantized
+
+    # def _scale(self, zhat_normalized: Tensor) -> Tensor:
+    #     d = self._levels - 1
+    #     return zhat_normalized * d
+
+    # def _scale_inverse(self, zhat: Tensor) -> Tensor:
+    #     d = self._levels - 1
+    #     return zhat / d
+
+    # def codes_to_indices(self, zhat: Tensor) -> Tensor:
+    #     """Converts a `code` to an index in the codebook."""
+    #     zhat = self._scale(zhat)
+    #     return (zhat * self._basis).sum(dim=-1).to(torch.int32)
+
+    # def indices_to_codes(self, indices: Tensor) -> Tensor:
+    #     """Inverse of `codes_to_indices`."""
+
+    #     indices = rearrange(indices, "... -> ... 1")
+    #     codes_non_centered = (indices // self._basis) % self._levels
+    #     codes = self._scale_inverse(codes_non_centered)
+
+    #     return codes
+
+    # def forward(self, z: Tensor) -> Tensor:
+    #     codes = self.quantize(z)
+    #     indices = self.codes_to_indices(codes)
+    #     diff = torch.tensor(0.0).cuda().float()
+
+    #     return codes, diff, indices
