fix entropy_loss bug, add kl loss

vector_quants/quantizers/base_vector_quantizer.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 
+import torch
 from torch import nn
 
-from .utils import compute_dist, entropy_loss, pack_one
+from .utils import compute_dist, entropy_loss_fn, kl_loss_fn, pack_one
 
 
 class BaseVectorQuantizer(ABC, nn.Module):
@@ -31,19 +32,40 @@ class BaseVectorQuantizer(ABC, nn.Module):
     def indice_to_code(self, indice):
         pass
 
+    def get_dist(self, latent):
+        if isinstance(self.codebook, nn.Module):
+            codebook = self.codebook.weight
+        else:
+            codebook = self.codebook
+        # (b, *, d) -> (n, d)
+        latent, ps = pack_one(latent, "* d")
+        dist = compute_dist(latent, codebook)
+
+        return dist
+
     def entropy_loss(self, latent=None, dist=None):
+        # E[H(p)] - H[E(p)]
+        if not hasattr(self, "entropy_loss_weight") or self.entropy_loss_weight == 0:
+            loss = torch.tensor(0.0).cuda().float()
         assert (
             latent is not None or dist is not None
         ), "At least one of latent or dist needs to be specified."
         if dist is None:
-            if isinstance(self.codebook, nn.Module):
-                codebook = self.codebook.weight
-            else:
-                codebook = self.codebook
-            # (b, *, d) -> (n, d)
-            latent, ps = pack_one(latent, "* d")
-            dist = compute_dist(latent, codebook)
+            dist = self.get_dist(latent)
+        loss = entropy_loss_fn(-dist, self.entropy_temperature, self.entropy_loss_type)
 
-        loss = entropy_loss(dist, self.entropy_temperature, self.entropy_loss_type)
+        return loss
+
+    def kl_loss(self, latent=None, dist=None):
+        # kl(p || uniform) = entropy(p) + c
+        if not hasattr(self, "kl_loss_weight") or self.kl_loss_weight == 0:
+            loss = torch.tensor(0.0).cuda().float()
+        assert (
+            latent is not None or dist is not None
+        ), "At least one of latent or dist needs to be specified."
+        if dist is None:
+            dist = self.get_dist(latent)
+
+        loss = kl_loss_fn(-dist)
 
         return loss

vector_quants/quantizers/gumbel_vector_quantizer.py
@@ -16,14 +16,14 @@ class GumbelVectorQuantizer(BaseVectorQuantizer):
         num_embed = cfg.num_embed
         embed_dim = cfg.embed_dim
         commitment_loss_weight = cfg.commitment_loss_weight
-        temp = cfg.temp
+        kl_temperature = cfg.kl_temperature
         kl_loss_weight = cfg.kl_loss_weight
         # get params end
 
         self._num_embed = num_embed
         self.embed_dim = embed_dim
         self.commitment_loss_weight = commitment_loss_weight
-        self.temp = temp
+        self.kl_temperature = kl_temperature
         self.kl_loss_weight = kl_loss_weight
 
         # init codebook
@@ -45,11 +45,11 @@ class GumbelVectorQuantizer(BaseVectorQuantizer):
         # quantize
         x_quant = self.indice_to_code(indice)
 
-        # compute diff
-        # diff = self.commitment_loss_weight * F.mse_loss(x_quant.detach(), x) + kl_loss
-        codebook_loss = kl_loss
+        # compute codebook loss
+        codebook_loss = self.commitment_loss_weight * F.mse_loss(x_quant.detach(), x)
         loss_dict = {
             "codebook_loss": codebook_loss,
+            "kl_loss": kl_loss,
         }
 
         x_quant = x + (x_quant - x).detach()
@@ -60,24 +60,13 @@ class GumbelVectorQuantizer(BaseVectorQuantizer):
         # (b, *, d) -> (n, d)
         latent, ps = pack_one(latent, "* d")
         # n, m
-        logits = -compute_dist(latent, self.codebook.weight)
+        dist = compute_dist(latent, self.codebook.weight)
         # n, m
         hard = False if self.training else True
-        indice = F.gumbel_softmax(logits, tau=self.temp, dim=-1, hard=hard)
+        indice = F.gumbel_softmax(-dist, tau=self.kl_temperature, dim=-1, hard=hard)
         indice = unpack_one(indice, ps, "* m")
 
-        if self.kl_loss_weight > 0:
-            # + kl divergence to the prior (uniform) loss, increase cb usage
-            # Note:
-            #       KL(P(x), Q(x)) = sum_x (P(x) * log(P(x) / Q(x)))
-            #       in this case: P(x) is p, Q(x) is uniform distribution (1 / num_embed)
-            p = F.softmax(logits, dim=-1)
-            kl_loss = (
-                self.kl_loss_weight
-                * torch.sum(p * torch.log(p * self.num_embed + eps), dim=-1).mean()
-            )
-        else:
-            kl_loss = torch.tensor(0.0).cuda().float()
+        kl_loss = self.kl_loss(latent=None, dist=dist)
 
         return indice, kl_loss
 

vector_quants/quantizers/lookup_free_quantizer.py
@@ -66,10 +66,7 @@ class LookUpFreeQuantizer(BaseVectorQuantizer):
         # compute codebook loss
         codebook_loss = self.commitment_loss_weight * F.mse_loss(x_quant.detach(), x)
 
-        if self.entropy_loss_weight > 0:
-            entropy_loss = self.entropy_loss(x)
-        else:
-            entropy_loss = torch.tensor(0.0).cuda().float()
+        entropy_loss = self.entropy_loss(x)
 
         loss_dict = {
             "codebook_loss": codebook_loss,

vector_quants/quantizers/utils.py
@@ -32,7 +32,36 @@ def round_ste(x):
     return x + (xhat - x).detach()
 
 
-def entropy_loss(affinity, temperature, loss_type="softmax"):
+# def entropy_loss_fn(affinity, temperature, loss_type="softmax", eps=1e-5):
+#     """
+#     Increase codebook usage by maximizing entropy
+
+#     affinity: 2D tensor of size Dim, n_classes
+#     """
+
+#     n_classes = affinity.shape[-1]
+
+#     affinity = torch.div(affinity, temperature)
+#     probs = F.softmax(affinity, dim=-1)
+#     log_probs = F.log_softmax(affinity + eps, dim=-1)
+
+#     if loss_type == "softmax":
+#         target_probs = probs
+#     elif loss_type == "argmax":
+#         codes = torch.argmax(affinity, dim=-1)
+#         one_hots = F.one_hot(codes, n_classes).to(codes)
+#         one_hots = probs - (probs - one_hots).detach()
+#         target_probs = one_hots
+#     else:
+#         raise ValueError("Entropy loss {} not supported".format(loss_type))
+
+#     avg_probs = torch.mean(target_probs, dim=0)
+#     avg_entropy = -torch.sum(avg_probs * torch.log(avg_probs + 1e-5))
+#     sample_entropy = -torch.mean(torch.sum(target_probs * log_probs, dim=-1))
+#     return sample_entropy - avg_entropy
+
+
+def entropy_loss_fn(affinity, temperature, loss_type="softmax", eps=1e-5):
     """
     Increase codebook usage by maximizing entropy
 
@@ -42,8 +71,8 @@ def entropy_loss(affinity, temperature, loss_type="softmax"):
     n_classes = affinity.shape[-1]
 
     affinity = torch.div(affinity, temperature)
-    probs = F.softmax(affinity, dim=-1)
-    log_probs = F.log_softmax(affinity + 1e-5, dim=-1)
+    log_probs = F.log_softmax(affinity + eps, dim=-1)
+    probs = torch.exp(log_probs)
 
     if loss_type == "softmax":
         target_probs = probs
@@ -59,3 +88,12 @@ def entropy_loss(affinity, temperature, loss_type="softmax"):
     avg_entropy = -torch.sum(avg_probs * torch.log(avg_probs + 1e-5))
     sample_entropy = -torch.mean(torch.sum(target_probs * log_probs, dim=-1))
     return sample_entropy - avg_entropy
+
+
+def kl_loss_fn(affinity, eps=1e-5):
+    # kl(p || uniform) = entropy(p) + c
+    log_probs = F.log_softmax(affinity + eps, dim=-1)
+    torch.exp(log_probs)
+    kl_loss = torch.mean(torch.sum(torch.exp(log_probs) * log_probs, dim=-1))
+
+    return kl_loss